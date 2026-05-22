7085689d96f46f221186ed69b7c00c90b909e90e
Salzig666 <salz.de.mar@gmail.com>
2026-05-22 16:20:33 -0400

    Update framework.js

--- js/framework.js
+++ js/framework.js
@@ -1,16 +1,15 @@
-// El núcleo de tu propio framework web
+
 export class SalzigFramework {
     constructor(rootId) {
         this.root = document.getElementById(rootId);
         this.components = {};
     }
 
-    // Registra un nuevo componente visual (una función que escupe HTML)
+
     registerComponent(name, renderFn) {
         this.components[name] = renderFn;
     }
 
-    // Renderiza el componente en la pantalla pasándole un estado de datos
     render(componentName, state = {}) {
         if (this.components[componentName]) {
             this.root.innerHTML = this.components[componentName](state);
